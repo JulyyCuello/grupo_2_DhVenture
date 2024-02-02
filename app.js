50221cd9515473d354cbc520d44ed975a2b0c5b2
JavierPalacios1723 <javierpalacios257@gmail.com>
2024-02-02 01:47:43 -0300

    se realizaron cambios en archivos

--- app.js
+++ app.js
@@ -8,33 +8,9 @@ app.use(express.static('public'))
 
 app.get('/',(req,res) => {
   res.sendFile(path.join(__dirname,'./views/index.html'))
-  
+
 })
 
-app.get('/home', (req, res) => {
-  res.redirect('/');
-})
-
-app.get('/micros.html',(req,res) => {
-  res.sendFile(path.join(__dirname,'./views/micros.html'))
-})
-
-app.get('/monitores.html',(req,res) => {
-  res.sendFile(path.join(__dirname,'./views/monitores.html'))
-})
-
-app.get('/notbooks.html',(req,res) => {
-  res.sendFile(path.join(__dirname,'./views/notbooks.html'))
-})
-
-app.get('/pcEscritorio.html',(req,res) => {
-  res.sendFile(path.join(__dirname,'./views/pcEscritorio.html'))
-})
-
-
-app.get('/placas.html',(req,res) => {
-  res.sendFile(path.join(__dirname,'./views/placas.html'))
-})
 
 app.get('/productCart.html',(req,res) => {
   res.sendFile(path.join(__dirname,'./views/productCart.html'))
