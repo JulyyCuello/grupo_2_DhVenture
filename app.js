3bb49c953fe4f314eef5dab23be2ccdc03f3b535
Marcos <marcosbrqz87@gmail.com>
2024-02-03 01:13:13 -0300

    subo algunos cambios

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const path = require("path")
 const port = 3030
 
 
-
-app.listen(port, ()=>{
-    console.log(`corriendo http://localhost:${port}`)
-})
+app.use(express.static("public")) 
 
 app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname,"./views/index.html"))
 })
 
-app.use(express.static("public")) 
-
-
+app.listen(port, ()=>{
+  console.log(`corriendo http://localhost:${port}`)
+})
