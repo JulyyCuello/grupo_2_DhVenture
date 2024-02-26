b25bbcd1c6cc551f605e8c967839d0b526846a8f
JavierPalacios1723 <javierpalacios257@gmail.com>
2024-02-25 23:43:27 -0300

    cambios en archivos

--- app.js
+++ app.js
@@ -3,34 +3,55 @@ const app = express()
 const path = require("path")
 const port = 3030
 
+    /*RUTES*/ 
+const authRoutes = require('./routes/authentication.routes');
+const prodRoutes = require('./routes/products.routes');
+const cartRoutes = require('./routes/cart.routes');
+const homeRoutes = require('./routes/index.routes');
+const errorRoutes = require('./routes/error.routes');
 
+ /* CONFIGS */
 app.use(express.static("public")) 
 
-app.get("/", (req, res) =>{
+/* ROUTERS */
+app.use("/", homeRoutes)
+app.use("/auten", authRoutes)
+app.use("/productos", prodRoutes)
+app.use("/carrito", cartRoutes)
+app.use("/error", errorRoutes)
+
+/*app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname,"./views/index.html"))
-})
+})*/
 
 
-app.get("/productCart", (req, res) =>{
+/*app.get("/productCart", (req, res) =>{
     res.sendFile(path.join(__dirname,"./views/productCart.html"))
-})
+})*/
 
 
-app.get("/productDetail", (req, res) =>{
+/*app.get("/productDetail", (req, res) =>{
     res.sendFile(path.join(__dirname,"./views/productDetail.html"))
-})
+});*/
 
 
-app.get('/register', (req,res) => {
+/*app.get('/register', (req,res) => {
     res.sendFile(path.join(__dirname,"./views/register.html"))
-});
+});*/
 
-app.get('/login', (req,res) => {
+/*app.get('/login', (req,res) => {
     res.sendFile(path.join(__dirname,"./views/login.html"))
-});
+});*/
 
-app.listen(port, ()=>{
+
+
+
+
+  /* MIDDLEWARE */
+
+
+
+  /* SERVER */
+    app.listen(port, ()=>{
     console.log(`corriendo http://localhost:${port}`)
-  })
-  
-  
+    })
---
+++ routes/authentication.routes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router(); 
+const path = require("path");
+const authController = require('../controllers/authentication')
+
+
+/*  (/auten) */ 
+router.get('/inicio', authController.login);
+
+router.get('/registro',authController.register);
+
+
+
+
+
+module.exports = router;
