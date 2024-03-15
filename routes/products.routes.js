23c9bae34acb7dbeff39faad35802b158fe84e59
Jorge segura <ramiropipi1815@gmail.com>
2024-03-14 23:49:11 -0300

    crud eliminar producto

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const productsControllers = require('../controllers/products/products.controller');
 
-router.get('/dashboard', productsControllers.dashboard);
+router.get('/agregar-producto', productsControllers.create);
 
+router.get("/dashboard",productsControllers.dashboard)
 router.post("/agregar-producto", productsControllers.agregarProducto);
+
+router.delete('/delete/:id', productsControllers.destroy);
 
 module.exports = router;
